003c008c8b4db893116a10ef208a0e744e718ae0
AtliHL <35534771+AtliHL@users.noreply.github.com>
2018-11-09 23:26:32 +0000

    Database
    Uppfærði gagnagrunn, er með inserts fyrir placeholder gögn

--- src/main/java/project/Database/db.sql
+++ src/main/java/project/Database/db.sql
@@ -7,13 +7,35 @@ CREATE DATABASE adstodQuestions;
 
 USE adstodQuestions;                      # Make sure to indicate what database to use
 
-CREATE TABLE Questions(                   # Table holding all questions
+CREATE TABLE QuestionsICE(                   # Table holding all questions in icelandic
   ID BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
   QuestionText VARCHAR(255),
   OptionCount INT(2)
 );
 
-CREATE TABLE Options(                     # Table holding all possible answers to questions
+CREATE TABLE QuestionsENG(                   # Table holding all questions in english
+  ID BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+  QuestionText VARCHAR(255),
+  OptionCount INT(2)
+);
+
+CREATE TABLE QuestionsPOL(                   # Table holding all questions in polish
+  ID BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+  QuestionText VARCHAR(255),
+  OptionCount INT(2)
+);
+
+CREATE TABLE OptionsICE(                     # Table holding all possible answers to questions
+  ID BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+  OptionText VARCHAR(255)
+);
+
+CREATE TABLE OptionsENG(                     # Table holding all possible answers to questions
+  ID BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+  OptionText VARCHAR(255)
+);
+
+CREATE TABLE OptionsPOL(                     # Table holding all possible answers to questions
   ID BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
   OptionText VARCHAR(255)
 );
@@ -31,9 +53,7 @@ CREATE TABLE PhoneNumbers(                # Table holding phone numbers for all 
 CREATE TABLE AssistanceResources(         # Table holding names and links of all assistance resources that can pop up in results
   ID BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
   ResourceName VARCHAR(255),
-  Link TEXT,
-  PhoneNumberID BIGINT(20),
-  CONSTRAINT FOREIGN KEY (PhoneNumberID) REFERENCES PhoneNumbers(ID)
+  Link TEXT
 );
 
 CREATE TABLE Results(                   # Table holding all possible results from answer sets
@@ -41,6 +61,20 @@ CREATE TABLE Results(                   # Table holding all possible results fro
   ResultTitle VARCHAR(255),
   AnswerSetID BIGINT(20),
   CONSTRAINT FOREIGN KEY (AnswerSetID) REFERENCES AnswerSets(ID)
+);
+
+/**
+ * This table connects the PhoneNumbers table to the AssistanceResources table
+ * Each assistance resource can have many phone numbers
+ * Each phone number can belong to multiple assistance resources
+ * This makes the connection table useful for reducing redundant data
+ */
+CREATE TABLE PhoneNumbersForResources(
+  ID BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+  PhoneNumberID BIGINT(20),
+  AssistanceResourceID BIGINT(20),
+  CONSTRAINT FOREIGN KEY (PhoneNumberID) REFERENCES PhoneNumbers(ID),
+  CONSTRAINT FOREIGN KEY (AssistanceResourceID) REFERENCES AssistanceResources(ID)
 );
 
 /*
@@ -58,15 +92,79 @@ CREATE TABLE ResourcesForResults(
 );
 
 /*
- * This table connects the Options table to the Questions table
+ * These tables connect the Options tables to the Questions tables
  * Each question has multiple options
  * Each option can be used in multiple questions
- * This makes the connection table useful for reducing redundant data
+ * This makes the connection tables useful for reducing redundant data
  */
-CREATE TABLE OptionsForAnswers(
+CREATE TABLE OptionsForAnswersICE(
   ID BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
   QuestionID BIGINT(20),
   OptionID BIGINT(20),
-  CONSTRAINT FOREIGN KEY (QuestionID) REFERENCES Questions(ID),
-  CONSTRAINT FOREIGN KEY (OptionID) REFERENCES Options(ID)
+  CONSTRAINT FOREIGN KEY (QuestionID) REFERENCES QuestionsICE(ID),
+  CONSTRAINT FOREIGN KEY (OptionID) REFERENCES OptionsICE(ID)
 );
+
+CREATE TABLE OptionsForAnswersENG(
+  ID BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+  QuestionID BIGINT(20),
+  OptionID BIGINT(20),
+  CONSTRAINT FOREIGN KEY (QuestionID) REFERENCES QuestionsENG(ID),
+  CONSTRAINT FOREIGN KEY (OptionID) REFERENCES OptionsENG(ID)
+);
+
+CREATE TABLE OptionsForAnswersPOL(
+  ID BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+  QuestionID BIGINT(20),
+  OptionID BIGINT(20),
+  CONSTRAINT FOREIGN KEY (QuestionID) REFERENCES QuestionsPOL(ID),
+  CONSTRAINT FOREIGN KEY (OptionID) REFERENCES OptionsPOL(ID)
+);
+
+
+-- Inserts
+INSERT INTO QuestionsICE (QuestionText, OptionCount) VALUES ("Kyn?", 3);
+INSERT INTO QuestionsENG (QuestionText, OptionCount) VALUES ("Gender?", 3);
+INSERT INTO QuestionsPOL (QuestionText, OptionCount) VALUES ("PLACEHOLDER?", 3);
+
+INSERT INTO OptionsICE (OptionText) VALUES ("Karlkyns");
+INSERT INTO OptionsICE (OptionText) VALUES ("Kvenkyns");
+INSERT INTO OptionsICE (OptionText) VALUES ("Annað");
+
+INSERT INTO OptionsENG (OptionText) VALUES ("Male");
+INSERT INTO OptionsENG (OptionText) VALUES ("Female");
+INSERT INTO OptionsENG (OptionText) VALUES ("Other");
+
+INSERT INTO OptionsPOL (OptionText) VALUES ("PHMale");
+INSERT INTO OptionsPOL (OptionText) VALUES ("PHFemale");
+INSERT INTO OptionsPOL (OptionText) VALUES ("PHOther");
+
+INSERT INTO AnswerSets (AnswerOptionSet) VALUES ("1");
+INSERT INTO AnswerSets (AnswerOptionSet) VALUES ("2");
+INSERT INTO AnswerSets (AnswerOptionSet) VALUES ("3");
+
+INSERT INTO PhoneNumbers(Number) VALUES ("+3545465645");
+INSERT INTO PhoneNumbers(Number) VALUES ("+3545656565");
+INSERT INTO PhoneNumbers(Number) VALUES ("+3545555555");
+
+INSERT INTO AssistanceResources(ResourceName, Link) VALUES ("Addi-kun", "addikun.is");
+INSERT INTO AssistanceResources(ResourceName, Link) VALUES ("Atli-chan", "atlichan.com");
+INSERT INTO AssistanceResources(ResourceName, Link) VALUES ("Tommi-san", "tommisan.po");
+
+INSERT INTO Results(ResultTitle, AnswerSetID) VALUES ("Male gender", 1);
+INSERT INTO Results(ResultTitle, AnswerSetID) VALUES ("Female gender", 2);
+INSERT INTO Results(ResultTitle, AnswerSetID) VALUES ("Other gender", 3);
+
+INSERT INTO PhoneNumbersForResources(PhoneNumberID, AssistanceResourceID) VALUES (1, 1);
+INSERT INTO PhoneNumbersForResources(PhoneNumberID, AssistanceResourceID) VALUES (2, 2);
+INSERT INTO PhoneNumbersForResources(PhoneNumberID, AssistanceResourceID) VALUES (3, 3);
+
+INSERT INTO ResourcesForResults(ResultID, AssistanceResourceID) VALUES (1, 1);
+INSERT INTO ResourcesForResults(ResultID, AssistanceResourceID) VALUES (2, 2);
+INSERT INTO ResourcesForResults(ResultID, AssistanceResourceID) VALUES (3, 3);
+
+INSERT INTO OptionsForAnswersICE (QuestionID, OptionID) VALUES (1, 1);
+
+INSERT INTO OptionsForAnswersENG (QuestionID, OptionID) VALUES (1, 1);
+
+INSERT INTO OptionsForAnswersPOL (QuestionID, OptionID) VALUES (1, 1);
