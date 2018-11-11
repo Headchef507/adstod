69ce83067e226ad1297a4b9e5b9a9098075fed2b
AtliHL <35534771+AtliHL@users.noreply.github.com>
2018-11-11 21:13:43 +0000

    Comments og code review
    Kláraði comments á Result entity
    
    Bætti við Vision and Scope Document (Assignment 1) og UML Class og Sequence Diagrams (Assignment 3) fyrir code review

--- src/main/java/project/persistence/entities/Result.java
+++ src/main/java/project/persistence/entities/Result.java
@@ -9,6 +9,7 @@ import javax.persistence.Id;
 //@Table(name = "postitnote") // If you want to specify a table name, you can do so here
 public class Result {
 
+    //Initializing variables
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
@@ -16,23 +17,29 @@ public class Result {
     private String title;
     private AssistanceResource[] assistanceResource;
 
+    // Empty constructor, if necessary to get an empty result object
     public Result (){
 
     }
 
+    // Constructor
     public Result(String title, AssistanceResource[] assistanceResource){
         this.assistanceResource = assistanceResource;
         this.title = title;
     }
 
+    // Getters and setters
+    // ID of result in database
     public int getId() { return id; }
 
     public void setId(int id) { this.id = id; }
 
+    // Title of result in database
     public String getTitle() { return title; }
 
     public void setTitle(String title) { this.title = title; }
 
+    // Array of assistanceResources from the Result
     public AssistanceResource[] getAssistanceResource() { return assistanceResource; }
 
     public void setAssistanceResource(AssistanceResource[] assistanceResource) { this.assistanceResource = assistanceResource; }
